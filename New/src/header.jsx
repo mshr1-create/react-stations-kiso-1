163cb0c24c8edc4d57dad7ac33265b64c7c18968
mshr1-creat <19950786m@gmail.com>
2024-07-15 11:50:26 +0900

    掲示板の作成(2024/7/15)

---
+++ New/src/header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+      <div>
+        <header className='threadAPIheader'>掲示板</header>
+        <nav>スレッドをたてる</nav>  
+        </div>
+      
+    )
+  }
+  
+  export default Header
