6056038d8cf2142e812ef366e8a843e6fbbb07f9
mshr1-creat <19950786m@gmail.com>
2024-11-08 21:52:21 +0900

    vite.config.jsの`base:`の修正

--- New/vite.config.js
+++ New/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const repoName = 'react-stations-kiso-1';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-stations-kiso-1/'
+  base: `/${repoName}/`,
 })
