a75b1e2f8dbea85a56944dc797fe96d973a7140f
mshr1-creat <19950786m@gmail.com>
2024-11-13 21:53:44 +0900

    gh-pages の実装

--- New/src/App.jsx
+++ New/src/App.jsx
@@ -1,26 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import { Header } from "./header.jsx";
-import ThreadList from "./ThreadList.jsx";
-import ThreadCreate from "./ThreadCreate.jsx";
-import ThreadIdList from "./ThreadIdList.jsx";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+import  { Header }  from "./header.jsx";
+import ThreadList from './ThreadList.jsx';
+import ThreadCreate from './ThreadCreate.jsx';
+import ThreadIdList from './ThreadIdList.jsx';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Header />
-        <Routes>
-          {/*React Routerを使ってルーティングを設定*/}
-          <Route path="/" element={<ThreadList />} />
-          <Route path="/threads/new" element={<ThreadCreate />} />
-          <Route path="/threads/:thread_id" element={<ThreadIdList />} />{" "}
-          {/* 新しいルートを追加 */}
+        <Header/>
+        <Routes>{/*React Routerを使ってルーティングを設定*/}
+          <Route path="/" element={<ThreadList />} /> 
+          <Route path="/threads/new" element={<ThreadCreate />} /> 
+          <Route path="/threads/:thread_id" element={<ThreadIdList />} /> {/* 新しいルートを追加 */}
         </Routes>
       </div>
     </Router>
-  );
+  )
 };
 
 export default App;
--- New/vite.config.js
+++ New/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const repoName = 'react-stations-kiso-1';
+// const repoName = 'react-stations-kiso-1';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: `/react-stations-kiso-1/`,
 })
